d05e0cf19363b19c43c1e801aae3ad85ee83e942
Ahmad Abdelhameed <Abdelhameed9@yahoo.com>
2021-01-05 15:30:26 +0200

    Adding description about project in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -4,7 +4,7 @@ import numpy as np
 
 ### Description
 '''
-######            US Bike Share     #########
+######           Explore US Bikeshare Data     #########
 Over the past decade, bicycle-sharing systems have been growing in number and
 popularity in cities across the world. Bicycle-sharing systems allow users
 to rent bicycles on a very short-term basis for a price. This allows people
